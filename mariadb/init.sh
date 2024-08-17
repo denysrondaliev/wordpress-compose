81f76ee849b8716896a4535be17daed61ddaaa8a
denysrondaliev <denysrondaliev@gmail.com>
2024-08-17 17:44:01 +0300

    feat: added mysqld-exporter [skip ci]

--- mariadb/init.sh
+++ mariadb/init.sh
@@ -3,18 +3,18 @@
 # Create a new MySQL user for mysqld-exporter with a password set from MYSQLD_EXPORTER_PASSWORD environment variable
 # see https://github.com/prometheus/mysqld_exporter?tab=readme-ov-file#required-grants
 mariadb \
-    --user="root" \
-    --password="${MARIADB_ROOT_PASSWORD}" \
-    --execute="
+  --user="root" \
+  --password="${MARIADB_ROOT_PASSWORD}" \
+  --execute="
     CREATE USER 'exporter'@'%' 
       IDENTIFIED BY '${MYSQLD_EXPORTER_PASSWORD}' 
       WITH MAX_USER_CONNECTIONS 3;
 "
 
 mariadb \
-    --user="root" \
-    --password="${MARIADB_ROOT_PASSWORD}" \
-    --execute="
+  --user="root" \
+  --password="${MARIADB_ROOT_PASSWORD}" \
+  --execute="
     GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* 
       TO 'exporter'@'%';
 "
